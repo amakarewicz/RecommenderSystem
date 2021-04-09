78b549a516960e0f870041ee089aaf15cfd4c1ed
Piotr Rogula <piotr.r99@gmail.com>
2021-04-09 15:38:14 +0200

    dodany warunek dla user not in db

--- Piotr/model/main.py
+++ Piotr/model/main.py
@@ -5,7 +5,10 @@ import numpy as np
 
 if __name__ == "__main__":
     # get db with articles
-    art = get_db(r'C:\Users\a814811\OneDrive - Atos\RecommenderSystem\art_clean_wt_popul_authoroccurences.csv')
-    User1 = Popularity(1,art)
-    #print(User1.art_head())
+    art_db = get_db(r'C:\Users\a814811\OneDrive - Atos\RecommenderSystem\art_clean_wt_popul_authoroccurences.csv')
+    user_db = get_db(r'C:\Users\a814811\OneDrive - Atos\RecommenderSystem\readers.csv')
+
+    # print(user_db.iloc[:,0])
+    User1 = Popularity(-3,articles_db=art_db,user_db=user_db)
+    print(User1.head(User1.user_db))
     print(User1.reccom())
--- Piotr/model/popularity_model.py
+++ Piotr/model/popularity_model.py
@@ -13,19 +13,29 @@ class Popularity:
     :type arg: int
     '''
     def __init__(self,user_id,articles_db,user_db=None,art_limit=5):
-        self.users = user_id
+        self.user = user_id
         self.articles = articles_db
         self.limit=art_limit
         self.user_db=user_db
-        self.reccomended_articles = None
+        self.recommended = None
     
-    def art_head(self):
-        return self.articles.head()
+    def head(self,db):
+        return db.head()
+    
+    # zmienić to na class method ???
+    def select_if_no_userdb(self):
+        selected = self.articles.sort_values(by='popularity',ascending=False).head(self.limit)[['nzz_id']]
+        self.recommended = [item[0] for item in selected.values.tolist()]
+        return
 
     def reccom(self):
-        if self.user_db == None:
-            selected = self.articles.sort_values(by='popularity',ascending=False).head(self.limit)[['nzz_id']]
-            self.recommended = [item[0] for item in selected.values.tolist()]
-        # elif user not in user_db
+        '''wyniki systemu rekondacji'''
+        if self.user_db is None:
+            '''przypadek bez zaimplementowanej bazy użytkowników'''
+            self.select_if_no_userdb()
+        elif self.user not in self.user_db.iloc[:,0]:
+            '''przypadek zaimplementowanej bazy użytkowników, użytkownik nie jest w bazie'''
+            self.select_if_no_userdb()
         # else -> bazuje na popularności autorów też
+            '''przypadek zaimplementowanej bazy użytkownikow, użytkownik jest w bazie'''
         return self.recommended
