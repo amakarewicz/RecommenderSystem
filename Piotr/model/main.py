43f3b44658797eef06f30a4e967636b8c403c3d8
Piotr Rogula <piotr.r99@gmail.com>
2021-04-12 10:41:47 +0200

    rozbicie klasy na wo_author i wt_author

--- Piotr/model/main.py
+++ Piotr/model/main.py
@@ -1,5 +1,5 @@
 from some_functions import get_db
-from popularity_model import Popularity
+from popularity_model import *
 import pandas as pd
 import numpy as np 
 
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     # print(x)
     # x = x.iloc[:,1].tolist()
     # print(user_db[user_db['id'] == 3].iloc[:,1].tolist())
-    User1 = Popularity(user_id=2,articles_db=art_db,user_db=user_db,art_limit=8)
-    User2 = Popularity(user_id=-3,articles_db=art_db,user_db=user_db,art_limit=8)
-    User3 = Popularity(user_id=2,articles_db=art_db,art_limit=2)
-    print(User1.reccom())
-    print(User2.reccom())
-    print(User3.reccom())
+    User1 = Popularity_model_wo_author(user_id=2,articles_db=art_db,user_db=user_db)
+    User2 = Popularity_model_wo_author(user_id=-3,articles_db=art_db,user_db=user_db)
+    User3 = Popularity_model_wo_author(user_id=2,articles_db=art_db)
+    print(User1.recomm(limit=8))
+    print(User2.recomm(limit=8))
+    print(User3.recomm(limit=8))
--- Piotr/model/popularity_model.py
+++ Piotr/model/popularity_model.py
@@ -1,7 +1,7 @@
 from some_functions import get_db
 import pandas as pd
 
-class Popularity:
+class Popularity_model:
     '''
     Popularity object contains list of <art limit > 
     recommended articles based on user and articles database
@@ -12,10 +12,10 @@ class Popularity:
     :param art_limit: number of reccomended articles
     :type arg: int
     '''
-    def __init__(self,user_id,articles_db,user_db=None,art_limit=5):
+    def __init__(self,user_id,articles_db,user_db=None):
         self.user = user_id
         self.articles = articles_db
-        self.limit=art_limit
+        # self.limit=art_limit
         self.user_db=user_db
         self.recommended = None
     
@@ -24,12 +24,37 @@ class Popularity:
     
     @staticmethod
     def select_if_no_userdb(art_db,limit):
+        pass
+
+    @staticmethod
+    def select_if_userdb(art_db, user_db, user, limit):
+        pass
+
+    def recomm(self, limit):
+        '''wyniki systemu rekondacji (lista <limit> wyników)'''
+        if self.user_db is None:
+            '''przypadek bez zaimplementowanej bazy użytkowników'''
+            self.recommended = self.select_if_no_userdb(self.articles, limit)
+        elif self.user not in self.user_db.iloc[:,0]:
+            '''przypadek zaimplementowanej bazy użytkowników, użytkownik nie jest w bazie'''
+            self.recommended = self.select_if_no_userdb(self.articles, limit)
+        else:
+            self.recommended = self.select_if_userdb(self.articles, self.user_db, self.user, limit)
+            '''przypadek zaimplementowanej bazy użytkownikow, użytkownik jest w bazie'''
+        return self.recommended
+
+class Popularity_model_wo_author(Popularity_model):
+    '''przypadek bez uwzględnienia autora'''
+    @staticmethod
+    def select_if_no_userdb(art_db,limit):
+        '''metoda recomm dla przypadku <user not in database>'''
         selected = art_db.sort_values(by='popularity',ascending=False).head(limit)[['nzz_id']]
         recommended = [item[0] for item in selected.values.tolist()]
         return recommended
 
     @staticmethod
     def select_if_userdb(art_db, user_db, user, limit):
+        '''metoda recomm dla przypadku <user in database>'''
         user_articles = user_db[user_db['id'] == user].iloc[:,1].tolist()
         selected = art_db.sort_values(by='popularity',ascending=False) \
                    .head(limit + len(user_articles))[['nzz_id']].values.tolist()
@@ -37,15 +62,24 @@ class Popularity:
         recommended = [item[0] for item in selected if item[0] not in user_articles][:limit]    # wyrzucam powtórki
         return recommended
 
-    def reccom(self):
-        '''wyniki systemu rekondacji'''
-        if self.user_db is None:
-            '''przypadek bez zaimplementowanej bazy użytkowników'''
-            self.recommended = self.select_if_no_userdb(self.articles, self.limit)
-        elif self.user not in self.user_db.iloc[:,0]:
-            '''przypadek zaimplementowanej bazy użytkowników, użytkownik nie jest w bazie'''
-            self.recommended = self.select_if_no_userdb(self.articles, self.limit)
-        else:
-            self.recommended = self.select_if_userdb(self.articles, self.user_db, self.user, self.limit)
-            '''przypadek zaimplementowanej bazy użytkownikow, użytkownik jest w bazie'''
-        return self.recommended
+
+class Popularity_model_wt_author(Popularity_model):
+    '''przypadek z uwzględnieniem autora'''
+    @staticmethod
+    def select_if_no_userdb(art_db,limit):
+        '''metoda recomm dla przypadku <user not in database>'''
+        selected = art_db.sort_values(by='popularity',ascending=False).head(limit)[['nzz_id']]
+        recommended = [item[0] for item in selected.values.tolist()]
+        return recommended
+
+    @staticmethod
+    def select_if_userdb(art_db, user_db, user, limit):
+        '''metoda recomm dla przypadku <user in database>'''
+        user_articles = user_db[user_db['id'] == user].iloc[:,1].tolist()   # artykuły przeczytane
+
+        for item in user_articles:
+            print(item)
+        # selected = art_db.sort_values(by='popularity',ascending=False) \
+        #            .head(limit + len(user_articles))[['nzz_id']].values.tolist()
+        # recommended = [item[0] for item in selected if item[0] not in user_articles][:limit]    # wyrzucam powtórki
+        return None
