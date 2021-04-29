6731e12e43bc1300798d4bf5ee9fab84e8da8093
Tyborowski <kamil.tyborowski@atos.net>
2021-04-29 16:18:41 +0200

    klasa abstrakcyjna, testy i poprawa bledow

--- Kamil/code/cf_model_main.py
+++ Kamil/code/cf_model_main.py
@@ -8,7 +8,7 @@ class CF_model(Recommendation_model):
 
     MODEL_NAME = "collaborative filtering"
 
-    def fit(self, n_latent_factors=150, regularization=100.0, alpha=50.0, iterations=15):
+    def __init__(self, articles_db=None, user_db=None, n_latent_factors=150, regularization=100.0, alpha=50.0, iterations=15):
         model = implicit.als.AlternatingLeastSquares(
             factors=n_latent_factors,
             regularization=regularization,
@@ -16,7 +16,7 @@ class CF_model(Recommendation_model):
         )
 
         reader_article_matrix_df = pd.crosstab(
-            self.user_db["user_id"], self.user_db["nzz_id"]
+            user_db["user_id"], user_db["nzz_id"]
         ).fillna(0)
         self.article_ids = {k: v for k, v in enumerate(reader_article_matrix_df.columns)}
         self.reader_ids = {v: k for k, v in enumerate(reader_article_matrix_df.index)}
@@ -30,6 +30,9 @@ class CF_model(Recommendation_model):
         self.reader_article_csr_matrix = reader_article_csr_matrix
         self.model = model
 
+
+        super().__init__(articles_db=articles_db, user_db=user_db)
+
     def recommend(self, user_id, ignored=True, limit=5):
         # Get and sort the user's predictions
 
@@ -42,17 +45,16 @@ class CF_model(Recommendation_model):
         elif len(ignored) > 0:
             filter_already_liked_items = False
             articles_to_ignore = [k for k, v in self.article_ids.items() if v in ignored]
-
-        sorted_user_predictions = self.model.recommend(
-            self.reader_ids[user_id],
-            self.reader_article_csr_matrix,
-            N=limit,
-            filter_already_liked_items=filter_already_liked_items,
-            filter_items=articles_to_ignore,
-        )
-        sorted_user_predictions = [(self.article_ids[prediction[0]], prediction[1]) for prediction in sorted_user_predictions]
-        sorted_user_predictions_df = pd.DataFrame(sorted_user_predictions, columns=["nzz_id", "recommendation_strength"])
-
-        recommendations_list = sorted_user_predictions_df["nzz_id"].tolist()
-
+        if user_id in self.reader_ids:
+            sorted_user_predictions = self.model.recommend(
+                self.reader_ids[user_id],
+                self.reader_article_csr_matrix,
+                N=limit,
+                filter_already_liked_items=filter_already_liked_items,
+                filter_items=articles_to_ignore,
+            )
+            recommendations_list = [self.article_ids[prediction[0]] for prediction in sorted_user_predictions]
+        else:
+            recommendations_list = []
+        print(recommendations_list)
         return recommendations_list
